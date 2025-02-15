bbd70466951e3a12872cd68b4e18c08aa8b406b9
IsaacEV <isaac_escudero@euneiz.com>
2025-02-15 19:36:15 +0100

    Add: Horizontal movement for Loick

--- Louise_and_Loick/Assets/Scripts/Loick.cs
+++ Louise_and_Loick/Assets/Scripts/Loick.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Loick : MonoBehaviour
 {
+    
+
     [SerializeField] float jumpForce;
+    [SerializeField] float moveSpeed;
     private Rigidbody2D rb;
     private bool isGrounded;
+    
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,10 +21,25 @@ public class Loick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
+    }
+    private void FixedUpdate()
+    {
         if (Input.GetKey(KeyCode.Space) && isGrounded == true)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
-        } 
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            float moveInput = Input.GetAxisRaw("Horizontal");
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocityY);
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
