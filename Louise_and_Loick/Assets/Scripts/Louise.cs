3e6c61acdbf6fdc73f54e80471216a30116f9d9e
IsaacEV <isaac_escudero@euneiz.com>
2025-02-17 19:58:24 +0100

    Add: Jump for Louise

---
+++ Louise_and_Loick/Assets/Scripts/Louise.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Louise : MonoBehaviour
+{
+    [SerializeField] float jumpForce;
+    [SerializeField] float moveSpeed;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && isGrounded == true)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
