cbc3460dbb3926a0f32b6eaa1d9577f5831eee7d
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-02-22 11:37:01 +0100

    added: camera follows smoothly, but something streange happens. player mooves "strange"

--- Louise_and_Loick/Assets/Scripts/Camera_follow.cs
+++ Louise_and_Loick/Assets/Scripts/Camera_follow.cs
@@ -3,23 +3,16 @@ using UnityEngine;
 
 public class Camera_follow : MonoBehaviour
 {
-    [SerializeField] GameObject player1;
-    [SerializeField] GameObject player2;
-    [SerializeField] float bounds;
+ 
     [SerializeField] Vector3 offset;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
+    public float smoothTime;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        float dist = Vector3.Distance(player1.transform.position, player2.transform.position);
-        bounds = dist;
-        transform.position = offset + new Vector3(player1.transform.position.x, player1.transform.position.y + 5, player1.transform.position.z - 10);
-
-
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
