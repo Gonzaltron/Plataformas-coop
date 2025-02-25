8b71edfb66019a2ac7501bc0c636605a575f9dcb
IsaacEV <isaac_escudero@euneiz.com>
2025-02-25 13:56:01 +0100

    Fix: Box bugs

---
+++ Louise_and_Loick/Assets/Scripts/Box.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BoxPushable : MonoBehaviour
+{
+    private Rigidbody2D rbB;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rbB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Loick"))
+        {
+            rbB.bodyType = RigidbodyType2D.Static;
+        }
+        if (collision.gameObject.CompareTag("Louise"))
+        {
+            rbB.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Louise"))
+        {
+            rbB.bodyType = RigidbodyType2D.Static;
+        }
+    }
+}
