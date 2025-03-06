b14e145e030226a1e918ac5fb35dc8d3ab558fb5
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-03-06 18:57:13 +0100

    Some fixes are needed, but camera should auto adjust FOV when both characters separate

--- Louise_and_Loick/Assets/Scripts/Camera_follow.cs
+++ Louise_and_Loick/Assets/Scripts/Camera_follow.cs
@@ -8,11 +8,19 @@ public class Camera_follow : MonoBehaviour
     public float smoothTime;
     private Vector3 velocity = Vector3.zero;
     [SerializeField] private Transform target;
+    [SerializeField] private Transform P2;
+    public float maxFov;
+    public float minFov;
 
-    // Update is called once per frame
-    void Update()
+    
+    void fixedUpdate()
     {
         Vector3 targetPosition = target.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        Vector3.position getDistancex = (targetPositionx - P2positionx) * (targetPositionx - P2positionx);
+        Vector3.position getDistancey = (targetPositiony - P2positiony) * (targetPositiony - P2positiony),
+        Vector3.position getDistance = Mathf.sqrt(getDistancex + getDistancey);
+        float fov = Mathf.Lerp(minFov, maxFov, getDistance);
     }
 }
+
