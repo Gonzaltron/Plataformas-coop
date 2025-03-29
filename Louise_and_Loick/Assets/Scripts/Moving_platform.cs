35cfd3191f7d0fd18e769945d783e1378ba1fa83
IsaacEV <isaac_escudero@euneiz.com>
2025-03-29 19:01:06 +0100

    The code of Moving_platform is write with comments

--- Louise_and_Loick/Assets/Scripts/Moving_platform.cs
+++ Louise_and_Loick/Assets/Scripts/Moving_platform.cs
@@ -3,8 +3,10 @@ using UnityEngine.UIElements;
 
 public class Moving_platform : MonoBehaviour
 {
+    //We pass two points to delimit the platform
     [SerializeField] private Transform Position1, Position2;
     [SerializeField] private float _speed;
+    //We use it to change the movement of the platform
     private bool _switch = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,13 +19,15 @@ public class Moving_platform : MonoBehaviour
     {
         if (_switch == false)
         {
+            //We access to the position of this object and we move it to the Position1 object in every frame.  
             transform.position = Vector2.MoveTowards(transform.position, Position1.position, _speed * Time.deltaTime);
         }
         else if (_switch == true)
         {
+            //We access to the position of this object and we move it to the Position1 object in every frame.
             transform.position = Vector2.MoveTowards(transform.position, Position2.position, _speed * Time.deltaTime);
         }
-
+        //We change the value of _switch depending on the position of the platform
         if (transform.position == Position1.position)
         {
             _switch = true;
