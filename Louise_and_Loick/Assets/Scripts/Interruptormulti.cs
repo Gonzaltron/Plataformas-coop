eed12f267cc2e1c041bb48f53fcb0631bdbe4c1d
IsaacEV <isaac_escudero@euneiz.com>
2025-04-09 18:30:15 +0200

    Se ha creado otro script para los interruptores que abren más de una puerta.

---
+++ Louise_and_Loick/Assets/Scripts/Interruptormulti.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Interruptormulti : MonoBehaviour
+{
+    //Este interruptor solo se coloca cuando se necesitan abrir más de una puerta. Funciona como los interruptores normales.
+    public bool activated;
+    public Vector2 positionOn;
+    public Vector2 positionOff;
+    [SerializeField] public Transform Door;
+    public Vector2 doorOff;
+    public Vector2 doorOn;
+    [SerializeField] public Transform Door2;
+    public Vector2 doorOff2;
+    public Vector2 doorOn2;
+    [SerializeField] public Transform Door3;
+    public Vector2 doorOff3;
+    public Vector2 doorOn3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        activated = false;
+        this.transform.position = positionOff;
+        Door.transform.position = doorOff;
+        Door2.transform.position = doorOff2;
+        Door3.transform.position = doorOff3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated == true)
+        {
+            this.transform.position = positionOn;
+            Door.transform.position = doorOn;
+            Door2.transform.position = doorOn2;
+            Door3.transform.position = doorOn3;
+            activated = false;
+
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        activated = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        activated = false;
+        this.transform.position = positionOff;
+        Door.transform.position = doorOff;
+        Door2.transform.position = doorOff2;
+        Door3.transform.position = doorOff3;
+
+    }
+}
