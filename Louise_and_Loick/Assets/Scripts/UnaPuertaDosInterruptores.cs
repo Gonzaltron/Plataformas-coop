c453482452e1ba756d2d5d31f91490b5f7d73b47
IsaacEV <isaac_escudero@euneiz.com>
2025-04-12 13:47:56 +0200

    Se ha creado un prefab de una puerta que solo se abre pulsando dos interruptores.

---
+++ Louise_and_Loick/Assets/Scripts/UnaPuertaDosInterruptores.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class UnaPuertaDosInterruptores : MonoBehaviour
+{
+    public DosInterruptoresUnapuerta Valorguardado1;
+    public DosInterruptoresUnapuerta Valorguardado2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Valorguardado1.contador == 1 && Valorguardado2.contador == 1)
+        {
+            Valorguardado1.Door.transform.position = Valorguardado1.doorOn;
+        }
+        if (Valorguardado1.contador == 0 || Valorguardado2.contador == 0 || Valorguardado1.contador == 0 && Valorguardado2.contador == 0)
+        {
+            Valorguardado1.Door.transform.position = Valorguardado1.doorOff;
+        }
+    }
+}
