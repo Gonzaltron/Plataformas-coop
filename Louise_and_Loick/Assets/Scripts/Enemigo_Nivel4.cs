e5ff21f28d46d14d4bb18db3307e836aa0a3ea33
IsaacEV <isaac_escudero@euneiz.com>
2025-04-26 15:50:59 +0200

    Los enemigos caen recto.

--- Louise_and_Loick/Assets/Scripts/Enemigo_Nivel4.cs
+++ Louise_and_Loick/Assets/Scripts/Enemigo_Nivel4.cs
@@ -5,7 +5,7 @@ public class Enemigo_Nivel4 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float speed;
     public bool moveRight;
-
+    private Rigidbody2D rbE;
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -16,6 +16,11 @@ public class Enemigo_Nivel4 : MonoBehaviour
         else
         {
             transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+        }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 10f);
+        if (hit.collider != null)
+        {
+            rbE.linearVelocityX = 0;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
