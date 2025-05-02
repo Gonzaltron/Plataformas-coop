dc5bbb95ab20c350bb5f7441cd9cb785fc2ee287
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-05-02 11:59:30 +0200

    Valve working

--- Louise_and_Loick/Assets/Scripts/Valve.cs
+++ Louise_and_Loick/Assets/Scripts/Valve.cs
@@ -1,28 +1,53 @@
 using UnityEngine;
+using System.Collections;
 
 public class Valve : MonoBehaviour
 {
     public bool activated;
     public Vector2 ON;
     public Vector2 OFF;
+    bool ONcheck;
     [SerializeField] public Transform Steam;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         activated = false;
+        ONcheck = false;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (activated == true && Input.GetKey(KeyCode.E))
+        if (activated == true && Input.GetKeyDown(KeyCode.E))
         {
-            Steam.transform.position = ON;
+            if (ONcheck == true)
+            {
+                Steam.transform.position = OFF;
+                
+                activated = false;
+            }
+            else if (ONcheck == false)
+            {
+                Steam.transform.position = ON;
+         
+                activated = false;
+            }
             activated = false;
+            StartCoroutine(DelayTime2());
+            Debug.Log("Ha esperado");
+            if (ONcheck == true)
+            {
+                ONcheck = false;
+            }
+            else if (ONcheck == false)
+            {
+                ONcheck = true;
+            }
+            StartCoroutine(WaitForFrames(30));
         }
     }
-   
+
     void OnTriggerEnter2D(Collider2D other)
     {
         activated = true;
@@ -33,9 +58,23 @@ public class Valve : MonoBehaviour
         activated = true;
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    IEnumerator DelayTime()
     {
-        activated = false;
-        Steam.transform.position = OFF;
+        
+        yield return new WaitForSeconds(1.0f);
+    }
+
+    IEnumerator DelayTime2()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Iniciando movimiento del agua");
+    }
+
+    IEnumerator WaitForFrames(int frames)
+    {
+        for (int i = 0; i < frames; i++)
+        {
+            yield return null; 
+        }
     }
 }
