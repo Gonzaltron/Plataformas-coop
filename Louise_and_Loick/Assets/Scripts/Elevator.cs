4b52484820354eba28e2ef916e3b9d40910b232d
Oliver <oliver_herrero@euneiz.com>
2025-05-04 00:35:49 +0200

    animacion ascensor arreglada

--- Louise_and_Loick/Assets/Scripts/Elevator.cs
+++ Louise_and_Loick/Assets/Scripts/Elevator.cs
@@ -11,7 +11,7 @@ public class Elevator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
