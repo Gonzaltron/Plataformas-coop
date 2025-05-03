c52b086895eb81160d5488128ce650caf1f911a7
IsaacEV <isaac_escudero@euneiz.com>
2025-05-03 11:51:36 +0200

    Implementado el enemigo en el nivel 1

--- Louise_and_Loick/Assets/Scripts/Enemigo_Nivel4.cs
+++ Louise_and_Loick/Assets/Scripts/Enemigo_Nivel4.cs
@@ -6,16 +6,25 @@ public class Enemigo_Nivel4 : MonoBehaviour
     public float speed;
     public bool moveRight;
     private Rigidbody2D rbE;
+
+    private Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (moveRight)
+        if (moveRight) // Se mueve hacia la izquierda
         {
             transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+
+            animator.SetBool("IsRight", true); // Activar animación caminar a la izquierda
         }
-        else
+        else // Se mueve hacia la derecha
         {
             transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            animator.SetBool("IsRight", false); // Activar animación caminar a la derecha
         }
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 10f);
         if (hit.collider != null)
@@ -34,5 +43,16 @@ public class Enemigo_Nivel4 : MonoBehaviour
             moveRight = true;
         }
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Steam"))
+        {
+            moveRight = false;
+        }
+        else
+        {
+            moveRight = true;
+        }
     }
 }
