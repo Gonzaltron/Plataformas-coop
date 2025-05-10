dafcb25de6ecb11b85a2aa55e4313f2e89585461
IsaacEV <isaac_escudero@euneiz.com>
2025-05-10 12:36:39 +0200

    Se ha solucionado el bug que no dejaba que las cajas cayeran bien

--- Louise_and_Loick/Assets/Scripts/Box.cs
+++ Louise_and_Loick/Assets/Scripts/Box.cs
@@ -5,11 +5,13 @@ public class BoxPushable : MonoBehaviour
     private Rigidbody2D rbB;
     private bool isTouchingLoick = false;
     private float originalMass;
+    [SerializeField] GameObject [] c;
+    private bool allRaysNull;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rbB = GetComponent<Rigidbody2D>();
-        rbB.gravityScale = 1; // Asegura que la gravedad esté activada
+ 
         rbB.bodyType = RigidbodyType2D.Dynamic; // La caja comienza como dinámica
         originalMass = rbB.mass;
     }
@@ -17,35 +19,46 @@ public class BoxPushable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        allRaysNull = true;
+        
         if (!isTouchingLoick && rbB.bodyType != RigidbodyType2D.Dynamic) //Asegura que la caja empiece dinámica si no está tocando a Loick
         {
-            rbB.bodyType = RigidbodyType2D.Dynamic; 
+            rbB.bodyType = RigidbodyType2D.Dynamic;
             rbB.gravityScale = 1;
-            
+
         }
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 10f);
-        if (hit.collider != null)
+        foreach (GameObject g in c)
         {
-            rbB.linearVelocityX = 0;
-            rbB.linearVelocityY = -5;
+            RaycastHit2D hit = Physics2D.Raycast(g.transform.position, -Vector2.up, 10f);
+            Debug.DrawRay(g.transform.position, -Vector2.up, Color.yellow);
+
+            if (hit.collider == null)
+            {
+                allRaysNull = false;
+                break;
+            }
         }
+        if (allRaysNull)
+        {
+            rbB.linearVelocity = new Vector2(0, -7);
+        }
+
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
         if (collision.gameObject.CompareTag("Loick")) // Se convierte en estática solo cuando Loick la toca
         {
             rbB.mass = 10000000;
             isTouchingLoick = true;
-            rbB.gravityScale = 0; // Desactiva la gravedad cuando es estática
         }
         else if (collision.gameObject.CompareTag("Louise"))
         {
             rbB.mass = originalMass;
             isTouchingLoick = false;
-            rbB.gravityScale = 1;
         }
-        
+
     }
 }
