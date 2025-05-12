a9770e64bbf9d1bef615e3f0d838ca28d87dddb2
Oliver <oliver_herrero@euneiz.com>
2025-05-12 22:13:50 +0200

    implementada aniamcion cubo agua

--- Louise_and_Loick/Assets/Scripts/Cubo de agua boton.cs
+++ Louise_and_Loick/Assets/Scripts/Cubo de agua boton.cs
@@ -9,7 +9,8 @@ public class Cubodeaguaboton : MonoBehaviour
     public float delay;
     public float speed;
     bool contact = false;
-    
+
+    public bool animacionActiva = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,10 +22,11 @@ public class Cubodeaguaboton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (contact == true)
+        // Solo inicia la animación si no está ya activada
+        if (contact && !animacionActiva)
         {
             StartCoroutine(DelayTime());
-            contact = false;
+            animacionActiva = true; // Evita que la animación se inicie varias veces
         }
     }
 
@@ -32,26 +34,28 @@ public class Cubodeaguaboton : MonoBehaviour
     {
         if (other.CompareTag("Louise") || other.CompareTag("Loick"))
         {
-            contact = true;
+            contact = true;  // Activa la animación al entrar en el trigger
         }
     }
 
     IEnumerator DelayTime()
     {
-        Debug.Log("Iniciando movimiento del agua");
 
-        // Mueve el agua gradualmente hacia el destino
+        // Mueve el agua hacia el destino
         while (Vector2.Distance(agua.position, aguaDestino) > 0.1f)
         {
             agua.position = Vector2.MoveTowards(agua.position, aguaDestino, speed * Time.deltaTime);
             yield return null; // Espera un frame antes de continuar
         }
 
-
         yield return new WaitForSeconds(delay);
 
-
+        // Vuelve el agua a su posición original
         agua.position = aguaOrigen;
+
+        // Desactiva la animación de agua
+        animacionActiva = false;
+        contact = false;  
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -60,5 +64,11 @@ public class Cubodeaguaboton : MonoBehaviour
         {
             contact = false;
         }
+    }
+
+    // Devuelve el etsado del agua
+    public bool IsAguaOn()
+    {
+        return contact;
     }
 }
