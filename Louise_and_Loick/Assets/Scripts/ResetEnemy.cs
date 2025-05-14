4d04cd977219ed03e0e4f4ff1f2b010a7173eaf4
IsaacEV <isaac_escudero@euneiz.com>
2025-05-14 19:51:04 +0200

    Añadir llave restante

--- Louise_and_Loick/Assets/Scripts/ResetEnemy.cs
+++ Louise_and_Loick/Assets/Scripts/ResetEnemy.cs
@@ -7,13 +7,13 @@ public class ResetEnemy : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -23,3 +23,4 @@ public class ResetEnemy : MonoBehaviour
         }
 
     }
+}
