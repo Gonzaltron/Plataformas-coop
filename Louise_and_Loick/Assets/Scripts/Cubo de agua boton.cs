1f635e45bbccbf4a657ca7583500182e313d2106
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-05-19 23:13:10 +0200

    water has sound in theory, but bot testable because lack of animator

--- Louise_and_Loick/Assets/Scripts/Cubo de agua boton.cs
+++ Louise_and_Loick/Assets/Scripts/Cubo de agua boton.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Cubodeaguaboton : MonoBehaviour
 {
-
+    bool SonidoAgua = false;
     public Vector2 aguaOrigen;
     public Vector2 aguaDestino;
     [SerializeField] public Transform agua;
@@ -32,14 +32,14 @@ public class Cubodeaguaboton : MonoBehaviour
             StartCoroutine(DelayTime());
             animacionActiva = true; // Evita que la animación se inicie varias veces
         }
-       /* if((Vector2)agua.transform.position == aguaDestino)
+        if((Vector2)agua.transform.position == aguaDestino)
         {
             if(SonidoAgua == false)
             {
                 SonidoAgua = true;
                 StartCoroutine(aguasonido());
             }
-        }*/
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -47,7 +47,7 @@ public class Cubodeaguaboton : MonoBehaviour
         if (other.CompareTag("Louise") || other.CompareTag("Loick"))
         {
             animator.SetBool("activado",true);
-            agua.position = aguaOrigen;
+            agua.transform.position = Vector2.MoveTowards(agua.transform.position, aguaDestino, speed * Time.deltaTime);
             contact = true;  // Activa la animación al entrar en el trigger
         }
     }
@@ -59,7 +59,7 @@ public class Cubodeaguaboton : MonoBehaviour
         while (Vector2.Distance(agua.position, aguaDestino) > 0.1f)
         {
             agua.position = Vector2.MoveTowards(agua.position, aguaDestino, speed * Time.deltaTime);
-            yield return null; // Espera un frame antes de continuar
+            yield return new WaitForSeconds(delay); // Espera antes de continuar
         }
         // Vuelve el agua a su posición original
         agua.position = aguaOrigen;
@@ -86,4 +86,12 @@ public class Cubodeaguaboton : MonoBehaviour
 
     }
 
+    IEnumerator aguasonido()
+    {
+        var aguaAudio = GetComponent<AudioSource>();
+        aguaAudio.Play();
+        yield return new WaitForSeconds(10.0f);
+        aguaAudio.Stop();
+        SonidoAgua = false;
+    }
 }
