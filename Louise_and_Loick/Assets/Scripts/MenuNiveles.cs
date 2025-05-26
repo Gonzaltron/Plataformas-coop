0dc4385333d5e0205c396eac7d078465dff994b4
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-05-26 22:24:28 +0200

    Menu niveles funcional

--- Louise_and_Loick/Assets/Scripts/MenuNiveles.cs
+++ Louise_and_Loick/Assets/Scripts/MenuNiveles.cs
@@ -5,30 +5,37 @@ public class MenuNiveles : MonoBehaviour
     public void Nivel1()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel_1");
+        Time.timeScale = 1; //Asegura que el tiempo del juego se reanude al iniciar un nuevo nivel
     }
     public void Nivel2()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel_2");
+        Time.timeScale = 1; //Asegura que el tiempo del juego se reanude al iniciar un nuevo nivel
     }
     public void Nivel3()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel_3");
+        Time.timeScale = 1; //Asegura que el tiempo del juego se reanude al iniciar un nuevo nivel
     }
     public void Nivel4()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel_4");
+        Time.timeScale = 1; //Asegura que el tiempo del juego se reanude al iniciar un nuevo nivel
     }
     public void Nivel5()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel_5");
+        Time.timeScale = 1; //Asegura que el tiempo del juego se reanude al iniciar un nuevo nivel
     }
     public void Nivel6()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel_6");
+        Time.timeScale = 1; //Asegura que el tiempo del juego se reanude al iniciar un nuevo nivel
     }
     public void Nivel7()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel_7");
+        Time.timeScale = 1; //Asegura que el tiempo del juego se reanude al iniciar un nuevo nivel
     }
     public void Back()
     {
